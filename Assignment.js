c9cf0473415d5660da7a2f2851bac12a8b57f181
Muhammad Uzair <153905110+MrUzair0@users.noreply.github.com>
2024-02-12 16:31:24 +0500

    JS

---
+++ Assignment.js
@@ -0,0 +1,24 @@
+
+var Password = "xyz123";
+var userPassword = prompt("Enter your password:");
+
+if (!userPassword) {
+    alert("Please enter your password");
+} else if (userPassword === Password) {
+    alert("Correct! The password you entered matches the original password");
+} else {
+    alert("Incorrect password");
+}
+
+{
+    var greeting;
+    var hour = 13;
+    if (hour < 18) {
+    greeting = "Good day";
+    }
+    else
+    {
+    greeting = "Good evening";
+    }
+    }
+    
